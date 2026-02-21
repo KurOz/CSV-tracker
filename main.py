23a64e49c4664fcb43d133f040eb7b0e4d01523d
KurOz <lauper.cyril@gmail.com>
2026-02-21 06:43:56 +0100

    first logic and menus

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pandas
+from menus import main_menu
 
+choice = 0
+while choice != str(3):
+    main_menu()
+    choice = input()
 
 df = pandas.read_csv("./test_file.csv", header=1)
 
---
+++ menus.py
@@ -0,0 +1,6 @@
+def main_menu():
+    print("CSV Tracker\n")
+    print("Please select your desired option")
+    print("1 - Create a new table")
+    print("2 - Open a table")
+    print("3 - Exit")
